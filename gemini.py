5eae9e6a2a5abd2210c2a9ed2123dfb6f3f9ff72
Tai Nguyen <Williamnguyen.dl@gmail.com>
2025-05-12 22:06:17 -0700

    change3

--- gemini.py
+++ gemini.py
@@ -42,7 +42,7 @@ skills_sheet = client.open("skillsets").sheet1
 last_row = len(sheet.col_values(1)) + 1
 next_row = len(skills_sheet.col_values(1)) + 1
 
-# 🧠 Initialize dsriver only once
+# 🧠 Initialize driver only once
 
 driver = setup_driver()
 if driver is None:
